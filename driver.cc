3fdce4485485ffb357f2fd772934192e730523d6
Tim <37099957+Trippindajive@users.noreply.github.com>
2023-08-29 20:04:47 -0600

    added root program

---
+++ driver.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout << "I am Encryptocon, the lord of simple substitution ciphers.\n"
+             << "My purpose is to help you with secret codes.\n"
+             << "It will involve a cipher wheel, an encryption table, a decryption"
+             << " table, an offset, and more.\n"
+             << "But no matter.\nAnd although there are many keys to try, I too hold my own secrets!\n"
+             << "To that end, user, let's start with some questions.\n"
+             << "WHAT SHALL BE THE OFFSET OF THY ENCRYPTION WHEEL?\n";
+        return 0;
+}
