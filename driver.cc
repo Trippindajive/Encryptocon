f3b11daaca1e5e73debf56347dff6e751fdedcc9
Tim <37099957+Trippindajive@users.noreply.github.com>
2023-08-31 22:28:45 -0600

    as

--- driver.cc
+++ driver.cc
@@ -9,7 +9,7 @@ int main()
              << "My purpose is to help you with secret codes.\n"
              << "It will involve a cipher wheel, an encryption table, a decryption"
              << " table, an offset, and more.\n"
-             << "But no matter.\nAnd although there are many keys to try, I too hold my own secrets!\n"
+             << "But no matter.\nAlthough there are many keys to try, I too hold my own secrets!\n"
              << "To that end, user, let's start with some questions.\n"
              << "WHAT SHALL BE THE OFFSET OF THY ENCRYPTION WHEEL?\n"
              << "-> ";
